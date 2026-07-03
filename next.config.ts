81a2d100a4b00143eb94f8257eb1f705797d07fd
Victor Polonio <victor.polonio@evolutionsoft.com.br>
2026-07-03 14:45:47 -0300

    chore:Atualiza pacotes de dependencia e gitIgnore

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
-const apiBaseUrl = process.env.API_BASE_URL ?? process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:5045";
+const apiBaseUrl =
+  process.env.API_BASE_URL ??
+  process.env.NEXT_PUBLIC_API_BASE_URL ??
+  'http://localhost:5045'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
@@ -10,11 +13,11 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/backend/:path*",
+        source: '/backend/:path*',
         destination: `${apiBaseUrl}/:path*`,
       },
-    ];
+    ]
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
